0a76cacfb47e7a01453ef2f6a3681e30b82b12d3
oscar ortiz <ortizg.oscar@gmail.com>
2020-08-14 22:43:02 -0300

    work in progress

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, redirect, render_template, request, url_for
+from flask_login import LoginManager, login_required, login_user, logout_user, login_required
 from flask_socketio import SocketIO, join_room
 
+from db import get_user
+from models.user import User
+
 app = Flask(__name__)
+app.secret_key = 'my_secret_key'
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user(username )
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message = 'Bad login request'
+
+    return render_template('login.html', message=message)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/chat')
 def chat():
@@ -24,9 +52,6 @@ def handle_send_message(data):
     socketio.emit('received_message',data, room=data['room'])
 
 
-
-
-
 @socketio.on('join_room')
 def handle_join_room_event(data):
     app.logger.info(f'{data["username"]} has joined the {data["room"]} room')
@@ -34,7 +59,9 @@ def handle_join_room_event(data):
     socketio.emit('join_room_notice', data)
 
 
-
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from werkzeug.security import  generate_password_hash
+from models.user import User
 
 client = MongoClient('mongodb+srv://admin:admin@cluster0.dzbqz.gcp.mongodb.net/ChatDB?retryWrites=true&w=majority')
 
@@ -10,3 +11,6 @@ def save_user(username, email, password):
     password_hash = generate_password_hash(password)
     users_collection.insert_one({'_id':username, 'email': email, 'password':password_hash})
 
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username })
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
